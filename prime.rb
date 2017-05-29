727cc55ed786ba6d524c29343c5feeeddd429e56
Alex Tung <acc.tung@gmail.com>
2017-05-29 01:03:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(num)
+  if num > 1
+    num_range = (2..num).to_a
+  else
+    return false
+  end
+  num_range.each do |x|
+    if num % x == 0 && num != x
+      return false
+    end
+  end
+  true
+
+end
